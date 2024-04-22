a4872cbc701b01589484816c147b92fba72ec69e
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2024-04-22 21:26:55 +0300

    main 01-02 has been added

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-57i_fb*=i1343%wtvz+6+%&g8hlmg!_tkf)e93mnubwl7zrdxb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'main',
 ]
 
 MIDDLEWARE = [
