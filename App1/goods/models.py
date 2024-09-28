83e97a9d051b4bf3fa3d9c2b0f7e3eb9169b8f2a
VLADIMIR KARTYSHOV <ikartyshov50@gmail.com>
2024-09-28 14:24:18 +0300

    03 38 простая регистрация моделей

--- App1/goods/models.py
+++ App1/goods/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categories(models.Model):  # как отображается в админ панели
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+
+    class Meta:
+        db_table = "category"  # как отображается в базе данных
+        verbose_name = "Категорию"  # переименовали в админ панеле
+        verbose_name_plural = "Категории"  # переименовали в админ панеле
